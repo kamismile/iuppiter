86f9601fd8b92ddcc3245a593e3ba663c3aa866d
mt <355860752@qq.com>
2013-09-18 15:58:44 +0800

    start

--- iuppiter/settings.py
+++ iuppiter/settings.py
@@ -1,4 +1,5 @@
 # Django settings for iuppiter project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -67,6 +68,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), '..', 'static').replace('\\','/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -102,7 +104,6 @@ ROOT_URLCONF = 'iuppiter.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'iuppiter.wsgi.application'
 
-import os
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), '..', 'templates').replace('\\','/'),)
 
 INSTALLED_APPS = (
